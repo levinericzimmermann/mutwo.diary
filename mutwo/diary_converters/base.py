2cb01289517e0206e4f3ac9f5082e7462f09fda5
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-22 18:51:44 +0100

    converters += base (extracted from modal)

---
+++ mutwo/diary_converters/base.py
@@ -0,0 +1,88 @@
+import logging
+import typing
+
+import numpy as np
+
+from mutwo import core_converters
+from mutwo import core_utilities
+from mutwo import diary_interfaces
+from mutwo import timeline_interfaces
+
+__all__ = (
+    "ContextTupleToEventPlacementTuple",
+)
+
+
+class ContextTupleToEventPlacementTuple(core_converters.abc.Converter):
+    def __init__(
+        self,
+        random_seed: int = 10,
+        **rquery_kwargs,
+    ):
+        rquery_kwargs.setdefault(
+            "entry_identifier",
+            "|".join(
+                tuple(
+                    map(
+                        lambda entry_class: entry_class.identifier,
+                        (
+                            diary_interfaces.Entry,
+                            diary_interfaces.DynamicEntry,
+                        ),
+                    )
+                )
+            ),
+        )
+        rquery_kwargs.setdefault(
+            "return_type", timeline_interfaces.EventPlacement.__name__
+        )
+
+        self._rquery_kwargs = rquery_kwargs
+        self._random = np.random.default_rng(random_seed)
+        self._logger = logging.getLogger(f"{__name__}.{type(self).__name__}")
+
+    def convert(
+        self, context_tuple: tuple[diary_interfaces.Context, ...]
+    ) -> tuple[timeline_interfaces.EventPlacement, ...]:
+
+        event_placement_list = []
+        context_identifier_to_entry_tuple = {}
+        for context in context_tuple:
+            try:
+                entry_tuple = context_identifier_to_entry_tuple[context.identifier]
+            except KeyError:
+                context_identifier_to_entry_tuple[
+                    context.identifier
+                ] = entry_tuple = self._context_to_entry_tuple(context)
+            entry_tuple = tuple(
+                filter(lambda entry: entry.is_supported(context), entry_tuple)
+            )
+            entry_relevance_tuple = tuple(e.relevance for e in entry_tuple)
+            if picked_entry := self._pick_entry(entry_tuple, entry_relevance_tuple):
+                self._logger.info(f"Picked '{picked_entry.name}'.")
+                if (event_placement := picked_entry(context)) is not None:
+                    event_placement_list.append(event_placement)
+            else:
+                self._logger.info("No entry picked.")
+
+        return tuple(event_placement_list)
+
+    def _context_to_entry_tuple(
+        self, context: diary_interfaces.Context
+    ) -> tuple[diary_interfaces.Entry, ...]:
+        return tuple(
+            diary_interfaces.fetch_wrapped_entry_tree().rquery(
+                context_identifier=str(context.identifier), **self._rquery_kwargs
+            )
+        )
+
+    def _pick_entry(
+        self,
+        entry_tuple: tuple[diary_interfaces.Entry, ...],
+        entry_relevance_tuple: tuple[float, ...],
+    ) -> typing.Optional[diary_interfaces.Entry]:
+        if entry_tuple:
+            return self._random.choice(
+                entry_tuple,
+                p=core_utilities.scale_sequence_to_sum(entry_relevance_tuple, 1),
+            )
--- mutwo/diary_converters/modal.py
+++ mutwo/diary_converters/modal.py
@@ -6,7 +6,7 @@ from mutwo import clock_converters
 from mutwo import clock_events
 from mutwo import core_converters
 from mutwo import core_events
-from mutwo import core_utilities
+from mutwo import diary_converters
 from mutwo import diary_interfaces
 from mutwo import music_parameters
 from mutwo import timeline_interfaces
@@ -54,36 +54,17 @@ class ModalSequentialEventToModalContextTuple(core_converters.abc.Converter):
 
 
 class Modal0SequentialEventToEventPlacementTuple(
-    clock_converters.Modal0SequentialEventToEventPlacementTuple
+    diary_converters.ContextTupleToEventPlacementTuple,
+    clock_converters.Modal0SequentialEventToEventPlacementTuple,
 ):
     def __init__(
         self,
+        *args,
         orchestration: music_parameters.Orchestration,
-        random_seed: int = 10,
         add_mod1: bool = True,  # Turn off modal1 mode if not needed for better performance
-        **rquery_kwargs,
+        **kwargs,
     ):
-        rquery_kwargs.setdefault(
-            "entry_identifier",
-            "|".join(
-                tuple(
-                    map(
-                        lambda entry_class: entry_class.identifier,
-                        (
-                            diary_interfaces.Entry,
-                            diary_interfaces.DynamicEntry,
-                        ),
-                    )
-                )
-            ),
-        )
-        rquery_kwargs.setdefault(
-            "return_type", timeline_interfaces.EventPlacement.__name__
-        )
-
-        self._orchestration = orchestration
-        self._rquery_kwargs = rquery_kwargs
-        self._random = np.random.default_rng(random_seed)
+        super().__init__(*args, **kwargs)
 
         self._modal0context = lambda seq: ModalSequentialEventToModalContextTuple(
             diary_interfaces.ModalContext0
@@ -97,29 +78,8 @@ class Modal0SequentialEventToEventPlacementTuple(
         )
         self._add_mod1 = add_mod1
 
-    def _context_to_entry_tuple(
-        self, context: diary_interfaces.Context
-    ) -> tuple[diary_interfaces.Entry, ...]:
-        return tuple(
-            diary_interfaces.fetch_wrapped_entry_tree().rquery(
-                context_identifier=str(context.identifier), **self._rquery_kwargs
-            )
-        )
-
-    def _pick_entry(
-        self,
-        entry_tuple: tuple[diary_interfaces.Entry, ...],
-        entry_relevance_tuple: tuple[float, ...],
-    ) -> typing.Optional[diary_interfaces.Entry]:
-        if entry_tuple:
-            return self._random.choice(
-                entry_tuple,
-                p=core_utilities.scale_sequence_to_sum(entry_relevance_tuple, 1),
-            )
-
     def convert(
-        self,
-        modal_0_sequential_event_to_convert,
+        self, modal_0_sequential_event_to_convert
     ) -> tuple[timeline_interfaces.EventPlacement, ...]:
         mod0seq = modal_0_sequential_event_to_convert
 
@@ -129,24 +89,4 @@ class Modal0SequentialEventToEventPlacementTuple(
             mod1seq = self._mod0seq2mod1seq(mod0seq)
             context_tuple += self._modal1context(mod1seq)
 
-        event_placement_list = []
-        context_identifier_to_entry_tuple = {}
-        print("\n\n")
-        for context in context_tuple:
-            try:
-                entry_tuple = context_identifier_to_entry_tuple[context.identifier]
-            except KeyError:
-                context_identifier_to_entry_tuple[
-                    context.identifier
-                ] = entry_tuple = self._context_to_entry_tuple(context)
-            entry_tuple = tuple(
-                filter(lambda entry: entry.is_supported(context), entry_tuple)
-            )
-            entry_relevance_tuple = tuple(e.relevance for e in entry_tuple)
-            if picked_entry := self._pick_entry(entry_tuple, entry_relevance_tuple):
-                print(f"Picked '{picked_entry.name}'.")
-                if (event_placement := picked_entry(context)) is not None:
-                    event_placement_list.append(event_placement)
-            else:
-                print("No entry picked.")
-        return tuple(event_placement_list)
+        return super().convert(context_tuple)
