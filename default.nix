cf5a513596de348cfea43a8fd8f3349ca100099a
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-27 22:03:50 +0200

    nix: Switch to niv based approach (just downloading tarball doesn't reliably update mutwo-nix)

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
+{ sources ? import ./nix/sources.nix}:
 let
-  sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
-  mutwo-diary = import (sourcesTarball + "/mutwo.diary/default.nix") {};
+  mutwo-diary = import (sources.mutwo-nix.outPath + "/mutwo.diary/default.nix") {};
   mutwo-diary-local = mutwo-diary.overrideAttrs (
     finalAttrs: previousAttrs: {
        src = ./.;
