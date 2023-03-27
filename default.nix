2b3e524a5fba966f7b53ecfafc7032bb8e407d88
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-03-27 21:47:37 +0200

    nix: Use mutwo-nix, but with local scrc

--- default.nix
+++ default.nix
@@ -1,34 +1,10 @@
-with import <nixpkgs> {};
-with pkgs.python310Packages;
-
-
 let
-
-  mutwo-clock-archive = builtins.fetchTarball "https://github.com/levinericzimmermann/mutwo.clock/archive/423baf0571b4485066ab82200effab5a38b74c86.tar.gz";
-  mutwo-clock = import (mutwo-clock-archive + "/default.nix");
-
+  sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
+  mutwo-diary = import (sourcesTarball + "/mutwo.diary/default.nix") {};
+  mutwo-diary-local = mutwo-diary.overrideAttrs (
+    finalAttrs: previousAttrs: {
+       src = ./.;
+    }
+  );
 in
-
-  buildPythonPackage rec {
-    name = "mutwo.diary";
-    src = fetchFromGitHub {
-      owner = "levinericzimmermann";
-      repo = name;
-      rev = "44c402d2cebaee9405e50a01cad1ebb940bd7151";
-      sha256 = "sha256-6cZh5K35XNl5unB/Aya/tdixXBoSsafoSd43/PzCRwo=";
-    };
-    checkInputs = [
-      python310Packages.pytest
-    ];
-    propagatedBuildInputs = [ 
-      python310Packages.zodb
-      mutwo-clock
-      python310Packages.numpy
-    ];
-    checkPhase = ''
-      runHook preCheck
-      pytest
-      runHook postCheck
-    '';
-    doCheck = true;
-  }
+  mutwo-diary-local
